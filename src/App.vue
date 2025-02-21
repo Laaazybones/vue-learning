e1321bafbf07b0d4400eff3c01dd47f359eabe89
Howsco <1120132909@qq.com>
2025-02-21 15:18:38 +0800

    引入animete.css动画库

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,18 @@
     <div id="root">
         <Test/>
         <Test2/>
+        <Test3/>
     </div>
 </template>
 
 <script>
 import Test from './components/Test.vue';
 import Test2 from './components/Test2.vue';
+import Test3 from './components/Test3.vue';
 
 export default {
     name: 'App',
-    components: {Test, Test2},
+    components: {Test, Test2, Test3},
 }
 </script>
 
---
+++ src/components/Test3.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <button @click="isShow = !isShow">显示/撤回</button>
+        <!-- 当一个transition中需要包含多个元素（即一个列表）时，需要使用transition-group，并且要给每个元素添加key值 -->
+        <transition-group 
+            appear
+            name="animate__animated animate__bounce"
+            enter-active-class="animate__zoomInUp"
+            leave-active-class="animate__zoomOutUp"
+        >
+            <h1 v-show="isShow" key="1">你好啊！</h1>
+            <h1 v-show="!isShow" key="2">啦啦啦！</h1>
+        </transition-group>
+    </div>
+</template>
+
+<script>
+import 'animate.css'
+
+export default {
+    name: 'Test',
+    data() {
+        return {
+            isShow: true
+        }
+    }
+}
+</script>
+
+<style scoped>
+h1 {
+    background-color: orange;
+}
+</style>
