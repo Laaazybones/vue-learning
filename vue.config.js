5636200558b6cd817ce490a29bce07a0266fd39b
Howsco <1120132909@qq.com>
2025-02-24 14:57:32 +0800

    引入axios及vue-cli代理服务器，进行请求代理。

--- vue.config.js
+++ vue.config.js
@@ -2,4 +2,7 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,  // 关闭自动语法检查
+  devServer: {
+    proxy: 'http://localhost:8080'  // 代理服务器地址
+  }
 })
